667a39d59d3401e92efc6a28a801739402433797
mayintao <545152072@qq.com>
2025-02-11 20:38:21 +0800

    Add files via upload

--- downloadvideo.py
+++ downloadvideo.py
@@ -36,7 +36,7 @@ def progress_hook(d):
         }
 
 
-#http://127.0.0.1:5000/api/tasks/downloadvideobyytnew/Iz9Gr1ATrDo
+# http://127.0.0.1:5000/api/tasks/downloadvideobyytnew/Iz9Gr1ATrDo
 @app.route("/api/tasks/downloadvideobyytnew/<string:videoId>", methods=["GET"])
 def downloadvideobyytnew(videoId):
     """ 启动后台线程下载视频 """
@@ -117,4 +117,5 @@ def downloadfile(videoId):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))  # 从环境变量读取端口，默认为 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
